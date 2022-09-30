26f1dc2bc8a44d794b6790cc48107c26ed3b735b
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2022-09-30 19:33:39 +0900

    Update UI.py

--- Socket/UI.py
+++ Socket/UI.py
@@ -9,44 +9,47 @@ class MyApp(QMainWindow):
         super().__init__()
         self.initUI()
 
+    # UI 실행
     def initUI(self):
-        self.windowSize()
+        self.window_size()
 
         self.statusBar().showMessage('Ready')
         self.statusBar()
 
-        self.createMenuBar(menu_name='하이')
-        self.createMenuBar()
+        self.create_menubar(menu_name='하이')
+        self.create_menubar()
 
         self.show()
 
-    def windowSize(self):
+    # 윈도우 크기
+    def window_size(self):
         self.setWindowTitle('chat')
         self.setWindowIcon(QIcon('images/titleicon.png'))
         self.resize(600, 800)
-        self.appPosition()
+        self.app_position()
 
-    def appPosition(self):
+    # 윈도우 생성 위치
+    def app_position(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    def createMenuBar(self, menu_name = "생성중"):
-        exitAction = QAction(QIcon('images/exit.png'), 'Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit App')
-        exitAction.triggered.connect(qApp.quit)
+    def create_menubar(self, menu_name = "생성중"):
+        exit_action = QAction(QIcon('images/exit.png'), 'Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit App')
+        exit_action.triggered.connect(qApp.quit)
 
-        menuBar = self.menuBar()
-        menuBar.setNativeMenuBar(False)
-        fileMenu = menuBar.addMenu(menu_name)
-        fileMenu.addAction(exitAction)
+        menu_bar = self.menuBar()
+        menu_bar.setNativeMenuBar(False)
+        file_menu = menu_bar.addMenu(menu_name)
+        file_menu.addAction(exit_action)
 
     # exitAction 함수로 만들기
     # 메뉴버튼 안에 들어갈 것들을 함수화하기.
     # 하이 생성중 둘다 안에 exit이기 떄문에 이걸 수정해야함.
-    def Action(self):
+    def action(self):
         pass
 
 
